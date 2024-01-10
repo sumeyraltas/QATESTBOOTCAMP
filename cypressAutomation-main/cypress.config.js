0196ec5491c71423d9f066a6ffaea17869ff08fd
sumeyra <sumeyraltas@gmail.com>
2024-01-10 21:05:57 +0300

    added manuel test results

--- cypressAutomation-main/cypress.config.js
+++ cypressAutomation-main/cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "3tqpk6",
+  projectId: 'is45do',
+  reporter: 'mochawesome',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
